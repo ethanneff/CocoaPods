af78c517c736b86da16e475451b455d2d790432a
Ethan Neff <ethan.neff@eneff.com>
2016-05-23 17:14:50 -0700

    updatd build settings

--- CocoaPods/ViewController.m
+++ CocoaPods/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <SAMGradientView/SAMGradientView.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	SAMGradientView *gradient = [[SAMGradientView alloc] initWithFrame:self.view.bounds]; // pull in the gradient class
+    
+    gradient.gradientColors = @[[UIColor redColor], [UIColor orangeColor]]; // set colors
+    [self.view addSubview:gradient]; // adds the gradient to the view
+    
 }
 
 - (void)didReceiveMemoryWarning
---
+++ Podfile
@@ -0,0 +1,1 @@
+pod 'SAMGradientView', '0.2.0'
